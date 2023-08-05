2d85b8963eedca39ab2258de2b15f5b4f1db6784
William <mauritius.william.wijaya1@gmail.com>
2023-08-05 17:19:00 +0700

    realtime method

--- socketio.ts
+++ socketio.ts
@@ -1,70 +1,65 @@
 import socketIo from 'socket.io';
 import { Server } from 'http';
 
+const rooms = new Map()
+
 export default function initializeSocketIO(httpServer: Server) {
   const io = new socketIo.Server(httpServer, {
     cors: {
-      origin: 'http://localhost:5173',
+      origin: 'http://127.0.0.1:5173',
       methods: ['GET', 'POST', 'PATCH', 'DELETE'],
       credentials: true,
     },
   });
 
-  const rooms = new Map();
-
-  function avaibleRoom(roomName: string, roomNumber: number) {
-    const room = roomName + roomNumber
-    const isAvaible = rooms.get(room) || []
-    if (isAvaible.length < 2) return {avaibleRoom: isAvaible, room}
-    else return avaibleRoom(roomName, roomNumber + 1)
-  }
-
-  function joinRoom(socket: any, roomName: string, roomNumber: number) {
-    const myRoom = avaibleRoom(roomName, roomNumber)
-    const roomOccupants = myRoom.avaibleRoom
-
-    console.log(rooms)
-    socket.join(myRoom.room);
-    
-    if (!rooms.get(myRoom.room)?.some(el => el === socket.id)) roomOccupants.push(socket.id); 
-    console.log(roomOccupants)
-    rooms.set(myRoom.room, roomOccupants);
-  }
-
   io.on('connection', (socket: any) => {
     console.log('A user connected');
 
+    let userRoom = null;
+
     socket.on('joinRoom', (data) => {
-      joinRoom(socket, data.room, data.number);
-      // io.emit()
-    });
+      for (const [room, users] of rooms.entries()) {
+        if (users.length < 2) {
+          userRoom = room;
+          users.push(socket);
+          socket.join(room);
+          io.to(room).emit('roomInfo', { roomName: room, users: users.length });
+          return;
+        }
+      }
+      const newRoom = Math.random().toString(36).substring(7);
+      userRoom = newRoom;
+      rooms.set(newRoom, [socket]);
+      socket.join(newRoom);
+      io.to(newRoom).emit('roomInfo', { roomName: newRoom, users: 1 });
+    })
 
     socket.on('message', (data) => {
-      console.log(data)
       io.to(data.room).emit('message', { username: data.username, message: data.message });
     });
 
     socket.on('disconnect', () => {
       console.log('A user disconnected');
-      cleanupUser(socket);
+      cleanupUser(userRoom, socket);
     });
   });
 
   // Cleanup user data when disconnecting
-  function cleanupUser(socket) {
-    for (const room of socket.rooms) {
-      if (room !== socket.id) {
-        const roomOccupants = rooms.get(room) || [];
-        const index = roomOccupants.indexOf(socket.id);
+  function cleanupUser(userRoom, socket) {
+    if (userRoom) {
+      const usersInRoom = rooms.get(userRoom);
+      if (usersInRoom) {
+        const index = usersInRoom.indexOf(socket);
         if (index !== -1) {
-          roomOccupants.splice(index, 1);
-          if (roomOccupants.length === 0) {
-            rooms.delete(room);
-          } else {
-            rooms.set(room, roomOccupants);
+          usersInRoom.splice(index, 1);
+          io.to(userRoom).emit('roomInfo', { roomName: userRoom, users: usersInRoom.length, disconnect: true });
+          if (usersInRoom.length === 0) {
+            rooms.delete(userRoom);
           }
         }
       }
     }
   }
+
+  return io
 }
