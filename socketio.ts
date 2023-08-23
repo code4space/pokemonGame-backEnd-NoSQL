448a4341a21b1697a2abac9235e626ff808445c9
William <mauritius.william.wijaya1@gmail.com>
2023-08-23 21:40:33 +0700

    fix send username with socket io

--- socketio.ts
+++ socketio.ts
@@ -16,29 +16,33 @@ export default function initializeSocketIO(httpServer: Server) {
     let userRoom = null;
 
     socket.on('joinRoom', (data) => {
-      console.log(data)
       for (const [room, users] of rooms.entries()) {
         if (users.length < 2) {
           userRoom = room;
           users.push(socket);
           socket.join(room);
-          io.to(room).emit('roomInfo', { roomName: room, users: users.length, username: data.username });
+          io.to(room).emit('roomInfo', { roomName: room, users: users.length });
           return;
         }
-        io.to(room).emit('roomInfo', { username: data.username });
       }
       const newRoom = Math.random().toString(36).substring(7);
       userRoom = newRoom;
       rooms.set(newRoom, [socket]);
       socket.join(newRoom);
-      io.to(newRoom).emit('roomInfo', { roomName: newRoom, users: 1, username: data.username });
+      io.to(newRoom).emit('roomInfo', { roomName: newRoom, users: 1});
     })
 
     socket.on('message', (data) => {
       io.to(data.room).emit('message', { username: data.username, message: data.message });
     });
 
+    socket.on('opponent-name', (data) => {
+      console.log(data, '==')
+      io.to(data.room).emit('opponent-name', { opponentName: data.username });
+    });
+
     socket.on('opponent-deck', (data) => {
+      console.log(data)
       io.to(data.room).emit('opponent-deck', { opponentDeck: data.opponentDeck, opponentName: data.opponentName });
     })
 
