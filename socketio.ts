6b853fe84dd39f885b2f6e9d7ec52820b8cb70f5
code4space <mauritius.william.wijaya1@gmail.com>
2023-08-23 16:55:15 +0700

    set deck for pvp

--- socketio.ts
+++ socketio.ts
@@ -13,33 +13,36 @@ export default function initializeSocketIO(httpServer: Server) {
   });
 
   io.on('connection', (socket: any) => {
-    console.log('A user connected');
-
     let userRoom = null;
 
     socket.on('joinRoom', (data) => {
+      console.log(data)
       for (const [room, users] of rooms.entries()) {
         if (users.length < 2) {
           userRoom = room;
           users.push(socket);
           socket.join(room);
-          io.to(room).emit('roomInfo', { roomName: room, users: users.length });
+          io.to(room).emit('roomInfo', { roomName: room, users: users.length, username: data.username });
           return;
         }
+        io.to(room).emit('roomInfo', { username: data.username });
       }
       const newRoom = Math.random().toString(36).substring(7);
       userRoom = newRoom;
       rooms.set(newRoom, [socket]);
       socket.join(newRoom);
-      io.to(newRoom).emit('roomInfo', { roomName: newRoom, users: 1 });
+      io.to(newRoom).emit('roomInfo', { roomName: newRoom, users: 1, username: data.username });
     })
 
     socket.on('message', (data) => {
       io.to(data.room).emit('message', { username: data.username, message: data.message });
     });
 
+    socket.on('opponent-deck', (data) => {
+      io.to(data.room).emit('opponent-deck', { opponentDeck: data.opponentDeck, opponentName: data.opponentName });
+    })
+
     socket.on('disconnect', () => {
-      console.log('A user disconnected');
       cleanupUser(userRoom, socket);
     });
   });
